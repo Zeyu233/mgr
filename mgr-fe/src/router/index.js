ce2fe240ae56826be1550937a3b7a94798f04d62
zy <867624339@qq.com>
2021-04-05 17:26:48 +0800

    完成基础布局

--- mgr-fe/src/router/index.js
+++ mgr-fe/src/router/index.js
@@ -4,13 +4,25 @@ const routes = [
   {
     path: '/auth',
     name: 'Auth',
-    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue'),
+    component: () => import(/* webpackChunkName: "Auth" */'../views/Auth/index.vue'),
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/',
+    name: 'BasicLayout',
+    component: () => import('../layout/BasicLayout/index.vue'),
+    children:[
+      {
+        path: 'books',
+        name: 'Books',
+        component: () => import(/* webpackChunkName: "Auth" */'../views/Books/index.vue'),
+      },
+      {
+        path: 'users',
+        name: 'User',
+        component: () => import(/* webpackChunkName: "Auth" */'../views/Books/index.vue'),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
